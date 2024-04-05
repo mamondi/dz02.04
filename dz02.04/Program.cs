ec4c9430d55ff19a1369135993de148cca1cb1a2
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-05 18:36:41 +0300

    done

--- dz02.04/Program.cs
+++ dz02.04/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+namespace dz02._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Це текст для перевірки, чи містить він задане слово.";
+            string wordToCheck = "задане";
+
+            Func<string, string, bool> containsWord = (inputText, word) =>
+                inputText.ToLower().Contains(word.ToLower());
+
+            Console.WriteLine($"Текст містить слово '{wordToCheck}': {containsWord(text, wordToCheck)}");
+        }
+    }
+}
