a18e0a6a7a46adba6ad47e2a37a5d46b63395e46
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-05 16:51:23 +0300

    done

---
+++ dz02.04/BP.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backpack
+{
+    internal class BP
+    {
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Material { get; set; }
+        public double Weight { get; set; }
+        public double Capacity { get; } = 10;
+        private double currentVolume;
+        private List<Item> contents;
+        private Action<Item> onItemAdded;
+
+        public BP(string color, string brand, string material, double weight, int capacity, Action<Item> onItemAdded = null)
+        {
+            Color = color;
+            Brand = brand;
+            Material = material;
+            Weight = weight;
+            Capacity = capacity;
+            contents = new List<Item>();
+            this.onItemAdded = onItemAdded;
+        }
+
+        public void AddItem(Item item)
+        {
+            if (currentVolume + item.Volume > Capacity)
+            {
+                throw new InvalidOperationException("Рюкзак повний. Неможливо додати новий предмет.");
+            }
+
+            contents.Add(item);
+            currentVolume += item.Volume;
+            onItemAdded?.Invoke(item);
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public double Volume { get; set; }
+
+        public Item(string name, double volume)
+        {
+            Name = name;
+            Volume = volume;
+        }
+    }
+}
--- dz02.04/Program.cs
+++ dz02.04/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using Backpack;
+
+namespace dz02._04
+{
+    class Program
+    {
+        static void Main()
+        {
+            BP backpack = new BP("black", "Nike", "polyester", 0.5, 20);
+            Console.WriteLine($"New backpack created -> {backpack.Color} {backpack.Brand}");
+            Item item = new Item("book", 1);
+
+            try
+            {
+                backpack.AddItem(item);
+                Console.WriteLine($"New item added to backpack -> {item.Name}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
