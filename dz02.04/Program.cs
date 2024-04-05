2452505286cd42b2ce84834e751bdfa9e5d257bf
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-05 18:32:28 +0300

    done

--- dz02.04/Program.cs
+++ dz02.04/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+namespace LambdaExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            int[] numbers = { 1, -2, 3, -4, 5, -6, 7, -8, 9, -10 };
+
+            Func<int[], int> countPositiveNumbers = arr => Array.FindAll(arr, num => num > 0).Length;
+
+            Console.WriteLine($"Кількість позитивних чисел у масиві: {countPositiveNumbers(numbers)}");
+        }
+    }
+}
