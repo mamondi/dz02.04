1082154872b1fa2cee067bba6e5ea5999dbb98b4
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-05 18:35:09 +0300

    done

--- dz02.04/Program.cs
+++ dz02.04/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace dz02._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { -2, 3, -4, 3, -6, -2, 7, -8, 9, -10 };
+
+            Func<int[], IEnumerable<int>> uniqueNegativeNumbers = arr =>
+            {
+                HashSet<int> uniqueNegatives = new HashSet<int>();
+                foreach (var num in arr)
+                {
+                    if (num < 0)
+                    {
+                        uniqueNegatives.Add(num);
+                    }
+                }
+                return uniqueNegatives;
+            };
+
+            Console.WriteLine("Унікальні негативні числа у масиві:");
+            foreach (var num in uniqueNegativeNumbers(numbers))
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
